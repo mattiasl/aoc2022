adda79885a656626fd43d0280973d8a4250bab3b
Mattias Liljeström <mattias.liljestrom@tietoevry.com>
2022-12-02 09:05:21 +0100

    day 02 - refactor names

--- src/main/clojure/day02.clj
+++ src/main/clojure/day02.clj
@@ -5,7 +5,7 @@
 (def strategy-1 {"A X" (+ 1 3) "A Y" (+ 2 6) "A Z" (+ 3 0) "B X" (+ 1 0) "B Y" (+ 2 3) "B Z" (+ 3 6) "C X" (+ 1 6) "C Y" (+ 2 0) "C Z" (+ 3 3)})
 (def strategy-2 {"A X" (+ 3 0) "A Y" (+ 1 3) "A Z" (+ 2 6) "B X" (+ 1 0) "B Y" (+ 2 3) "B Z" (+ 3 6) "C X" (+ 2 0) "C Y" (+ 3 3) "C Z" (+ 1 6)})
 
-(defn scores [input score] (reduce (fn [a c] (+ a (score c))) 0 input))
+(defn scores [strategy-guide strategy] (reduce (fn [a c] (+ a (strategy c))) 0 strategy-guide))
 
 (scores strategy-guide strategy-1)
 (scores strategy-guide strategy-2)
