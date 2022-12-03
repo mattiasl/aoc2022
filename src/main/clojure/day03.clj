823b23ed019417af9cf1fb5fb36abc442894244a
Mattias Liljeström <mattias.liljestrom@tietoevry.com>
2022-12-03 23:08:40 +0100

    day 03 - small adjustments

--- src/main/clojure/day03.clj
+++ src/main/clojure/day03.clj
@@ -4,24 +4,21 @@
 
 (def rucksacks (split (slurp "./src/main/clojure/input/day03.in") #"\n"))
 
-(defn priority [item]
-  (- (int item) (if (#(Character/isUpperCase %) item) 38 96)))
+(def priority (into {} (map-indexed (fn [i c] [c i])) "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
-(defn get-common-char [groups]
+(defn get-common-chars [groups]
   (->> (map set groups)
        (apply intersection)
-       (first)))                                            ; assume only one common
-
-(defn split-into-compartments [rucksack]
-  (partition (/ (count rucksack) 2) rucksack))
+       (into [])))
 
 (defn priorities [groups]
-  (->> (map get-common-char groups)
+  (->> (map get-common-chars groups)
+       (flatten)
        (map priority)
-       (apply +)))
+       (reduce +)))
 
 (defn part-1 [rucksacks]
-  (priorities (map split-into-compartments rucksacks)))
+  (priorities (map #(partition (/ (count %) 2) %) rucksacks)))
 
 (defn part-2 [rucksacks]
   (priorities (partition 3 rucksacks)))
