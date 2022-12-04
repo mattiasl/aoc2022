095d1b87230e6813172462ef6ecec05a77b7794b
Mattias Liljeström <mattias.liljestrom@tietoevry.com>
2022-12-04 10:27:31 +0100

    day 04

---
+++ src/main/clojure/day04.clj
@@ -0,0 +1,23 @@
+(ns day04
+  (:require [clojure.string :refer [split]]))
+
+(def assignments (split (slurp "./src/main/clojure/input/day04.in") #"\n"))
+
+(defn fully-contains? [[a b c d]]
+  (or (<= a c d b) (<= c a b d)))
+
+(defn overlaps? [[a b c d]]
+  (and (<= a d) (<= c b)))
+
+(defn parse-assignment [pair]
+  (->> (re-matches #"(\d+)-(\d+),(\d+)-(\d+)" pair)
+       (rest)
+       (map #(Integer/parseInt %))))
+
+(defn solve [assignments fn?]
+  (->> (map parse-assignment assignments)
+       (filter fn?)
+       (count)))
+
+(solve assignments fully-contains?)
+(solve assignments overlaps?)
